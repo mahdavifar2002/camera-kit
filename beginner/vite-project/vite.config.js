f5424615d99042572e5bff95fd2e8f4d23c85dde
mahdavifar2002 <mahdavifar2002@gmail.com>
2024-05-13 18:22:44 +0330

    Use certificate files for https

---
+++ beginner/vite-project/vite.config.js
@@ -0,0 +1,13 @@
+import fs from 'fs';
+import path from 'path';
+
+export default {
+  server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'keys/privkey.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'keys/fullchain.pem')),
+    },
+    // Make sure the server is accessible over the local network
+    host: '0.0.0.0',
+  },
+};
