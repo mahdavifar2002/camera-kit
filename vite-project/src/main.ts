92632c809995ecb257a9b15d9fe46e40975844d5
Ali Mahdavifar <mahdavifar2002@gmail.com>
2024-05-16 12:39:25 +0330

    Add built-in proxy feature for users without a VPN

--- vite-project/src/main.ts
+++ vite-project/src/main.ts
@@ -1,14 +1,36 @@
 import { bootstrapCameraKit, createMediaStreamSource, Transform2D } from "@snap/camera-kit"
 
 (async function() {
-
   const url = document.location.search;
   const urlParams = new URLSearchParams(url);
 
   const lensId = urlParams.get("id");
   const groupId = urlParams.get("group");
   const cameraMode = urlParams.get("camera") || "front";
+  const proxy = urlParams.get("proxy") || "false";
 
+  // Setting proxy server
+  if (proxy == "true") {
+    window.fetch = new Proxy(window.fetch, {
+      apply: function (target, thisArg, args) {
+        const baseUrl = `https://free-net.tech/`;
+
+        const url = args[0] instanceof Request ? args[0].url : args[0];
+
+        if (!url.toString().includes(baseUrl)) {
+          const fullUrl = baseUrl + url;
+          args = [fullUrl, ...args.slice(1)];
+        }
+
+        // Forward function call to the original fetch with the updated URL
+        const result = Reflect.apply(target, thisArg, args);
+
+        return result;
+      }
+    });
+  }
+
+  // Bootstrapping Camera Kit
   const cameraKit = await bootstrapCameraKit({
     apiToken: "eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzE1NTk4Mzk4LCJzdWIiOiI4MTk0MmI3Zi0xMDNhLTQ2OGUtOTQwNC1kNTcxMGQxM2NjOTl-U1RBR0lOR35iNTZjNWE2Zi1mZDAwLTRkMjMtYjI3Ni1jODZhMzg5ZDU2NWUifQ.1keEzLpu6Kr_mefm8i7H_Rqhgt_edoAqtXWmnDd7ANs",
   });
@@ -22,11 +44,11 @@ import { bootstrapCameraKit, createMediaStreamSource, Transform2D } from "@snap/
       facingMode: cameraMode == "front" ? "user" : "environment"
     },
   });
-  
+
   const source = createMediaStreamSource(mediaStream);
 
   await session.setSource(source);
-  
+
   if (cameraMode == "front")
     source.setTransform(Transform2D.MirrorX);
 
@@ -36,11 +58,11 @@ import { bootstrapCameraKit, createMediaStreamSource, Transform2D } from "@snap/
   );
 
   document.title = lens.name;
-  
+
   await session.applyLens(lens);
 
   await session.play();
-  
+
   document.getElementById("spinner")?.setAttribute("style", "display: none;");
   liveRenderTarget.classList.remove("loading");
 })();
